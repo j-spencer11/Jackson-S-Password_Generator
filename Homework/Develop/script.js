70d1dacf87f924b73aa8b451670187b878e4ff6c
j-spencer11 <jackspencer0806@gmail.com>
2021-09-20 11:44:50 -0500

    final code. added functionality for choosing what characters to be included, and added a cancel option at the start if one chooses to

--- Homework/Develop/script.js
+++ Homework/Develop/script.js
@@ -1,59 +1,116 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var symbols = [" !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
-var numbers = [" 1234567890"]
-var lowercase = [" qwertyuiopasdfghjklzxcvbnm"]
-var uppercase = [" QWERTYUIOPASDFGHJKLZXCVBNM"]
-
-
-/*    Pseudocoding----------
-  1. Need to know how many characters the password will have
-  2. Need to know are special characters included
-  3. Need to know if numbers are included
-  4. Need to know if uppercase letters are included
-  5. Need to know if lowercase letters are included
-
-  - Build final list of possible characters
-  6. We need a way to store all possible characters allowed in the password 
-
-  How can we build our password one letter at a time?
-    For each character needed, grab a random element from the allowed list
+// Starting Values
+var numberOfCharacters = 0;
+var specialCharsAllowed = false;
+var numbersAllowed = false;
+var lowerCaseAllowed = false;
+var upperCaseAllowed = false;
+function askAllTheQuestions() {
+  // Finding out length of password and cancelling prompt if choosing to
+  var askNumberOfCharacters = window.prompt("How many characters would you like in the password? (Min. of 8, Max. of 128");
+  console.log(askNumberOfCharacters)
+  numberOfCharacters = parseInt(askNumberOfCharacters);
+  if (!isNaN(numberOfCharacters)) {
 
 
 
+   
+    console.log(numberOfCharacters);
+
+    if (numberOfCharacters >= 8 && numberOfCharacters <= 128) {
 
 
-*/
 
-function askHowManyCharacters(){
-//....
-//...
+      // Finding out if special characters want to be included
+      specialCharsAllowed = window.confirm("Would you like special characters included? ('Ok' for yes. 'Cancel' for no)");
+
+      console.log(specialCharsAllowed);
+
+      //Finding out if numbers want to be included
+      numbersAllowed = window.confirm("Would you like numbers included? ('Ok' for yes. 'Cancel' for no)");
+
+      console.log(numbersAllowed);
+
+      //Finding out if lowercase letters want to be included
+      lowerCaseAllowed = window.confirm("would you like lowercase letters included? ('Ok' for yes. 'Cancel' for no)");
+
+      console.log(lowerCaseAllowed);
+
+      //Finding out if uppercase letters are allowed
+      upperCaseAllowed = window.confirm("Would you like uppercase letters included? ('Ok' for yes. 'Cancel' for no)");
+
+      console.log(upperCaseAllowed);
+    } else {
+      window.alert("Value must be between 8 and 128 characters!");
+      askAllTheQuestions();
+    }
+  }
+}
+//Functions for getting type of characters preferred
+
+function getSpecialCharacter() {
+  var symbols = "";
+  if (specialCharsAllowed) {
+    symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  }
+  return symbols;
 }
 
-function generatePassword(){
-  var numberOfCharacters = window.prompt ("How many characters do you want in your password? (8-128)");
-  var specialCharsAllowed = window.confirm ("Do you want special characters in your password?");
-  var numbersAllowed = window.confirm ("Do you want numbers in your password?");
-  var lowercaseAllowed = window.confirm ("Do you want lowercase letters in your password?");
-  var uppercaseAllowed = window.confirm ("Do you want uppercase letters in your password?");
-  
-numberOfCharacters = parseInt(numberOfCharacters);
-console.log (numberOfCharacters);
+function getNumber() {
+  var numbers = "";
+  if (numbersAllowed) {
+    numbers = "1234567890";
+  }
+  return numbers;
+}
+
+function getLowerCase() {
+  var lower = "";
+  if (lowerCaseAllowed) {
+    lower = "qwertyuiopasdfghjklzxcvbnm";
+  }
+  return lower;
+}
+
+function getUpperCase() {
+  var upper = "";
+  if (upperCaseAllowed) {
+    upper = "QWERTYUIOPASDFGHJKLZXCVBNM";
+  }
+  return upper;
+}
+
+function characterSet() {
+  var characters = (getSpecialCharacter() + getNumber() + getLowerCase() + getUpperCase());
+  return characters;
+}
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  askAllTheQuestions();
 
   var result = "";
-  for ( var i = 0; i < numberOfCharacters; i++ ) {
-    result += characterSet.charAt(Math.floor(Math.random() * 
-    numberOfCharacters));
- }
+  var allCharacters = characterSet();
+
+  for (var i = 0; i < numberOfCharacters; i++) {
+    result += allCharacters.charAt(Math.floor(Math.random() * allCharacters.length));
+    console.log(result)
+  }
+
   return result;
+
 }
+
+
 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
   passwordText.value = password;
 }
 
